12ba14f6227b79fb90f749486253e89e231f2e73
nguyen tien nghiep <nguyen_tien_nghiep@mediado.jp>
2018-07-24 17:44:58 +0900

    first commit

---
+++ dist/greet.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var sayHello = function (name) {
+    return "hello From\u3000" + name;
+};
+exports.default = sayHello;
+// export function sayHello(name: string) {
+//     return `Hello from ${name}`;
+// }
